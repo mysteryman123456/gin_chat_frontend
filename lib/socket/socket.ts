8b77522a7782faf2b936ea2c5cb0037d8f738853
mysteryman123456 <nschldhl61@gmail.com>
2026-02-22 23:42:47 +0545

    Added next public backend url in socket conection

--- lib/socket/socket.ts
+++ lib/socket/socket.ts
@@ -1,9 +1,6 @@
 import { io, type Socket } from "socket.io-client";
 
-export const socket: Socket = io(
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:8080",
-  {
-    autoConnect: false,
-    transports: ["websocket"],
-  }
-);
+export const socket: Socket = io(process.env.NEXT_PUBLIC_BACKEND_URL!, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
